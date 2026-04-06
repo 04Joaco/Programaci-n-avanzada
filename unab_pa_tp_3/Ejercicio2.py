47e84ec28021cf314ccf83c6b13c2122513e7b3a
Joaquin <134343008+04Joaco@users.noreply.github.com>
2026-04-06 17:36:43 -0300

    Implementación de la class Punto
    Crear la clase Punto con dos atributos x e y (ambos numéricos), con el correspondiente
    constructor que recibe ambos valores.

---
+++ unab_pa_tp_3/Ejercicio2.py
@@ -0,0 +1,30 @@
+class Punto:
+
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+
+    def eje_x(self):
+        return self.x
+
+    def eje_y(self):
+        return self.y
+
+    def impresion(self):
+        return "(" + str(self.x) + ", " + str(self.y) + ")"
+
+    def opuesto(self):
+        nuevo = Punto(self.x * -1, self.y * -1)
+        return nuevo
+
+=============== prueba
+
+  p1 = Punto(2, 3)
+
+print(p1.impresion())
+
+p2 = p1.opuesto()
+print(p2.impresion())
+
+print(p1.eje_x())
+print(p1.eje_y())
